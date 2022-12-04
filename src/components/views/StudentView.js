930dee6a925dcec0e267a52616dcfb8a3968c749
Kevin Hui <kevinhui98@gmail.com>
2022-12-04 14:20:05 -0500

    adding link to campus on student view

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,7 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
 const StudentView = (props) => {
   const { student } = props;
 
@@ -14,7 +15,9 @@ const StudentView = (props) => {
       <h1>Name: {student.firstname + " " + student.lastname}</h1>
       <h3>Email: {student.email}</h3>
       <h3>GPA: {student.gpa}</h3>
-      <h3>{student.campus.name}</h3>
+      <Link to={`/campus/${student.campusId}`}>
+        <h3>{student.campus.name}</h3>
+      </Link>
     </div>
   );
 
